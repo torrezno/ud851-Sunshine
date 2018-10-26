c936b662bb4521a85367aa32dba143fda378e5de
Vicente Lozano <vicentelozano@gmail.com>
2018-10-26 13:03:43 +0200

    S06.01 Add menu activity to Main and Detail Activities

--- S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -55,5 +55,17 @@ public class DetailActivity extends AppCompatActivity {
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_settings:
+                Intent intent = new Intent(this,SettingsActivity.class);
+                startActivity(intent);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     // TODO (7) Launch SettingsActivity when the Settings option is clicked
 }
